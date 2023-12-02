7024cbf377d4b572651f0bc0874ca4be57252a77
Rowan <82100519+Rowaaaaan@users.noreply.github.com>
2023-12-02 18:28:14 +0800

    add day 1 solution

---
+++ src/day1/src/main/java/xyz/rowaaaaan/aoc/Main.java
@@ -0,0 +1,18 @@
+package xyz.rowaaaaan.aoc;
+
+// Parse a text file line by line
+// Read the line until I find a number,
+// then break out of loop and loop again
+// starting from the end of the string
+//
+// If I return to the index of the first number,
+// add the number to itself.
+// 
+// TODO:
+// - [ ] Write file parser
+// - [ ] Write loop that reads the numbers in the string
+public class Day1 {
+    public static void main(String[] args) {
+
+    }
+}
